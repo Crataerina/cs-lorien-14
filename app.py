a0c366d636a8766d660ea8d984bf7bc7feec5550
Crataerina <aristocless@hotmail.com>
2021-06-13 11:52:20 +0200

    Update app.py

--- app.py
+++ app.py
@@ -43,7 +43,7 @@ app.layout = html.Div([
             value=[device for device in df.device.unique()],
             labelStyle={'display': 'inline-block'}
         )
-    ], style={"columnCount": 4, 'textAlign': "center", "margin-top": "24px", "margin-bottom": "48px"}),
+    ], style={"columnCount": 3, 'textAlign': "center", "margin-top": "24px", "margin-bottom": "48px"}),
     html.Div([
         html.Div([
             html.Img(src="https://upload.wikimedia.org/wikipedia/commons/9/99/Sample_User_Icon.png",
@@ -75,7 +75,7 @@ app.layout = html.Div([
         ]),
         html.Div([
             html.Img(src="https://logos-marcas.com/wp-content/uploads/2020/11/Twitch-Emblema-650x366.png",
-                     style={"width": "100px"}),
+                     style={"width": "50px"}),
             html.H2(
                 id='twitch-visit',
             )
